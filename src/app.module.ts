561e468bcfb93823bd34df229ad1823ccb1262a4
ahadddd <ahad.shehbaz19@gmail.com>
2023-07-20 15:55:52 +0500

    db connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksRepository } from './tasks/task.repository';
+import { Task } from './tasks/task.entity';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'task-management',
+      synchronize: true,
+      autoLoadEntities: true
+    })
+  ],
   controllers: [],
   providers: [],
 })
