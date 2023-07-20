ced7482bb166b97f11407bfc9ec5fc6a51296db5
ahadddd <ahad.shehbaz19@gmail.com>
2023-07-20 18:42:33 +0500

    sign up/sign in routes defined, strategy implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthCredentialsDTO } from './DTO/auth-credentials.dto';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(private authService: AuthService) { }
+
+    @Post('signup')
+    createUser(@Body() authCredentials: AuthCredentialsDTO): Promise<User> {
+        return this.authService.signUp(authCredentials);
+    }
+
+    @Post('signin')
+    signIn(@Body() authCredentials: AuthCredentialsDTO): Promise<{accessToken: string}> {
+        return this.authService.signIn(authCredentials);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { AuthCredentialsDTO } from './DTO/auth-credentials.dto';
+import { User } from './user.entity';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userRepository: UserRepository) { }
+
+    signUp(authCredentials: AuthCredentialsDTO): Promise<User> {
+        return this.userRepository.createUser(authCredentials);
+    }
+
+    signIn(authCredentials: AuthCredentialsDTO): Promise<{accessToken: string}>{
+        const {username, password} = authCredentials;
+        return this.userRepository.signIn(authCredentials);
+    }
+
+}
