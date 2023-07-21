0dbe5745df4a33316fd70d3ad465601c889bfe5d
ahadddd <ahad.shehbaz19@gmail.com>
2023-07-21 16:31:08 +0500

    AuthModule added

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,9 +4,11 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 import { TasksRepository } from './task.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     TypeOrmModule.forFeature([Task])
   ],
   controllers: [TasksController],
